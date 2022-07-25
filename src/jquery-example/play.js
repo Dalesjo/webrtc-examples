3a9d8fe3412cbc4742c4d70f2299ef94379dd470
Samuel Dalesjö <Dalesjo@users.noreply.github.com>
2022-07-25 23:16:40 +0200

    fixed play token not working when in url

--- src/jquery-example/play.js
+++ src/jquery-example/play.js
@@ -11,7 +11,7 @@ let state = {
     playSdpURL: "",
     playApplicationName: "",
     playStreamName: "",
-    applicationToken: ""
+    playApplicationToken: ""
   }
 };
 let statePrefix = 'play';
